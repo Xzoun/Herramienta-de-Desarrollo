99e14aac89676f30108603140508356f7bc90bdc
Leo Benitez <L96@live.com.ar>
2023-05-10 13:57:49 -0300

    prueba

--- JugateaAlgo/Viborita/juego 1.js
+++
@@ -1,9 +0,0 @@
-    const bienvenidainicial = document.getElementById("bienvenida");
-    bienvenidainicial.addEventListener("submit", function funcion(event) {
-        event.preventDefault();        
-        var nombre = document.getElementById("ingresonombre").value || "Player 1";
-        window.name = nombre
-        document.getElementById("indicacioninicial").style.display = "block"
-        document.getElementById("mododejuegobotones").style.display = "block"
-        document.getElementById("bienvenida").style.display = "none"
-    }, { once: true })   
