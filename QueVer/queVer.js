dcc5d6f195a5d6e0d0d0d27f859fbd19070d05ab
Leo Benitez <L96@live.com.ar>
2023-05-20 03:17:38 -0300

    Actualizacion

--- QueVer/queVer.js
+++ QueVer/queVer.js
@@ -1,18 +1,26 @@
-const trailerInicio = document.getElementById("botonBuscar");
-trailerInicio.addEventListener("click", () => {
-    const buscador = document.getElementById("buscador");
-    console.log("Click")
-    console.log(buscador)
-   
-    buscador.classList.toggle("active");
-    console.log(buscador)
-    trailerInicio.classList.toggle("active");
-});
-    window.addEventListener("scroll", ()=>{
-const banner = document.getElementById("banner");
-banner.classList.toggle("black");
-// const top = body.getBoungingClientRect().top();
-// if(banner.getBoungingClientRect().top()==body.getBoungingClientRect().top()){
+const lupita = document.getElementById("botonBuscar");
+const buscador = document.getElementById("buscador");
 
-// }
-    })
+lupita.addEventListener("click", () => {
+    buscador.classList.toggle("active");
+    lupita.classList.toggle("active");
+});
+
+const general = document.getElementById("general");
+general.addEventListener("click", () => {
+    lupita.classList.remove("active");
+    buscador.classList.remove("active");
+});
+
+window.addEventListener("scroll", () => {
+    const banner = document.getElementById("banner");
+    const scroll = window.scrollY;
+    buscador.classList.remove("active");
+    lupita.classList.remove("active");
+    if (scroll < 1) {
+        banner.style.background = "linear-gradient(180deg, rgba(0,0,0,0.9974836713812324) 18%, rgba(0,0,0,0) 76%)";
+    } else {
+        banner.style.background = "black";
+    }
+});
+
